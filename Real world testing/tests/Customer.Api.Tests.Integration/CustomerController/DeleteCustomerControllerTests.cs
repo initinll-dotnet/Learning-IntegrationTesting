68984841efbe7f0af4ee10939db7c9a854d158b9
Nitin Londhe <nitin.londhe@genmills.com>
2022-08-08 01:46:57 +0530

    DeleteCustomerControllerTests added for delete api calls

---
+++ Real world testing/tests/Customer.Api.Tests.Integration/CustomerController/DeleteCustomerControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Bogus;
+
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+
+using FluentAssertions;
+
+using System.Net;
+using System.Net.Http.Json;
+
+using Xunit;
+
+namespace Customer.Api.Tests.Integration.CustomerController;
+
+public class DeleteCustomerControllerTests : IClassFixture<CustomerApiFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.Email, faker => faker.Person.Email)
+        .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+        .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGithubUser)
+        .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
+
+    public DeleteCustomerControllerTests(CustomerApiFactory apiFactory)
+    {
+        _httpClient = apiFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsOk_WhenCustomerExists()
+    {
+        // Arrange
+        var customer = _customerGenerator.Generate();
+        var createdResponse = await _httpClient.PostAsJsonAsync("customers", customer);
+        var createdCustomer = await createdResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        // Act
+        var response = await _httpClient.DeleteAsync($"customers/{createdCustomer!.Id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsNotFound_WhenCustomerDoesNotExists()
+    {
+        // Act
+        var response = await _httpClient.DeleteAsync($"customers/{Guid.NewGuid()}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
